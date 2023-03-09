2e902fb3ca965d08bb779ed198b7d765a7fd39c5
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-03-09 17:06:49 +1100

    docs: Update tailwind config - colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yoles: '#E01922',
+      },
+    },
   },
   plugins: [],
 };
