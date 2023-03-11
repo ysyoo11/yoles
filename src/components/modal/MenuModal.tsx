9b9c74307ad9cfe94b531df9cdea21055e5906fd
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-03-12 03:23:29 +1100

    feat: Update MenuModal component

--- src/components/modal/MenuModal.tsx
+++ src/components/modal/MenuModal.tsx
@@ -2,9 +2,8 @@ import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { Fragment } from 'react';
 
-import menu from 'public/menu.json';
-
 import MenuDetails from '@/components/custom/MenuDetails';
+import menu from 'public/menu.json';
 
 interface Props {
   isOpen: boolean;
@@ -31,19 +30,22 @@ export default function MenuModal({ isOpen, onClose }: Props) {
             <Transition.Child
               as={Fragment}
               enter='ease-out duration-300'
-              enterFrom='-translate-x-[500px]'
+              enterFrom='-translate-x-full'
               enterTo='translate-x-0'
               leave='ease-in duration-200'
               leaveFrom='translate-x-0'
-              leaveTo='-translate-x-[500px]'
+              leaveTo='-translate-x-full'
             >
               <Dialog.Panel className='relative min-h-screen w-full transform overflow-hidden bg-white text-left align-middle shadow-xl transition-all'>
                 <div className='flex items-center justify-between border-b p-6'>
                   <Dialog.Title as='h3' className='text-2xl font-semibold'>
                     Menu
                   </Dialog.Title>
-                  <button className='rounded-full border border-transparent p-1.5 text-black hover:border-gray-700 hover:bg-gray-100'>
-                    <XMarkIcon onClick={onClose} className='h-5 w-5' />
+                  <button
+                    onClick={onClose}
+                    className='rounded-full border border-transparent p-1.5 text-black hover:border-gray-700 hover:bg-gray-100'
+                  >
+                    <XMarkIcon className='h-5 w-5' />
                   </button>
                 </div>
                 <div className='p-2'>
