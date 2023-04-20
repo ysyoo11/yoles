a24755a134dd21a95183b013b04d1c98b360a33e
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-04-20 17:01:06 +1000

    feat: Define product categories

---
+++ src/defines/category.ts
@@ -0,0 +1,35 @@
+export const meatSeafoodSubCategories = [
+  'beef-veal',
+  'lamb',
+  'pork',
+  'poultry',
+  'seafood',
+] as const;
+export const fruitVegetablesSubCategories = [
+  'fruit',
+  'nuts-dried-fruit',
+  'salad-herbs',
+  'vegetables',
+] as const;
+export const dairyEggsFridgeSubCategories = [
+  'cheese',
+  'dairy',
+  'eggs',
+  'milk',
+  'vegetarian-vegan',
+] as const;
+export const bakerySubCategories = [
+  'cakes-desserts',
+  'hot-cross-buns',
+  'packaged-breads',
+  'gluten-free-range',
+  'vegan-range',
+] as const;
+export const drinksSubCategories = [
+  'coffee-drinks',
+  'cold-drinks',
+  'energy-drinks',
+  'juice',
+  'water',
+] as const;
+export const frozenSubCategories = ['frozen-fruit', 'frozen-meals'] as const;
