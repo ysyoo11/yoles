cb9f1b5797fe902d571f43484d6faa35bbef4d2e
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-04-20 16:55:07 +1000

    docs: Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'productimages.coles.com.au',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
