7fb22c3fe0951b0949d93d155b2117a08541bcf6
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-04-22 23:55:18 +1000

    feat: Add trolleyItems global state in yoles-context

--- src/components/yoles-context.tsx
+++ src/components/yoles-context.tsx
@@ -1,24 +1,32 @@
 import {
+  Dispatch,
   ReactNode,
   Reducer,
+  SetStateAction,
   createContext,
   useContext,
   useEffect,
   useMemo,
   useReducer,
+  useState,
 } from 'react';
 
+import { Product } from '@/backend/product/model';
 import { SEARCH_HISTORY_KEY } from '@/constant/local-storage-key';
 
 export type YolesState = {
   searchHistory: string[];
+  trolleyItems: Product[];
 };
 
 const initialState: YolesState = {
   searchHistory: [],
+  trolleyItems: [],
 };
 
-type YolesAction = { type: 'SET_SEARCH_HISTORY'; searchHistory: string[] };
+type YolesAction =
+  | { type: 'SET_SEARCH_HISTORY'; searchHistory: string[] }
+  | { type: 'SET_TROLLEY_ITEMS'; trolleyItems: Product[] };
 
 export const YolesContext = createContext<YolesState>(initialState);
 
@@ -26,15 +34,19 @@ const yolesReducer: Reducer<YolesState, YolesAction> = (state, action) => {
   switch (action.type) {
     case 'SET_SEARCH_HISTORY':
       return { ...state, searchHistory: action.searchHistory };
+    case 'SET_TROLLEY_ITEMS':
+      return { ...state, trolleyItems: action.trolleyItems };
   }
 };
 
 type YolesStore = YolesState & {
   setSearchHistory: (searchHistory: string[]) => void;
+  setTrolleyItems: Dispatch<SetStateAction<Product[]>>;
 };
 
 export function YolesProvider({ children }: { children: ReactNode }) {
   const [state, dispatch] = useReducer(yolesReducer, initialState);
+  const [trolleyItems, setTrolleyItems] = useState<Product[]>([]);
 
   const setSearchHistory = (searchHistory: string[]) =>
     dispatch({ type: 'SET_SEARCH_HISTORY', searchHistory });
@@ -56,9 +68,11 @@ export function YolesProvider({ children }: { children: ReactNode }) {
   const value = useMemo<YolesStore>(
     () => ({
       ...state,
+      trolleyItems,
       setSearchHistory,
+      setTrolleyItems,
     }),
-    [state]
+    [state, trolleyItems]
   );
 
   return (
