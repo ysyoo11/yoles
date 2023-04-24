061946026ce8e445d8821c311704bc0abdd821c2
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-04-25 08:09:10 +1000

    feat: Apply NProgress in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import NextNProgress from 'nextjs-progressbar';
+
 import '@/assets/main.css';
 import BaseLayout from '@/components/layout/Base';
 import { YolesProvider } from '@/components/yoles-context';
@@ -8,11 +10,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).layout || BaseLayout;
 
   return (
-    <YolesProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </YolesProvider>
+    <>
+      <NextNProgress
+        color='#E01922'
+        showOnShallow={false}
+        height={2}
+        startPosition={0.3}
+        options={{ easing: 'ease', speed: 500, showSpinner: false }}
+      />
+      <YolesProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </YolesProvider>
+    </>
   );
 }
 
