f7fe728c7aa82eb7810701373958f8a7ed23f3ba
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-04-27 16:29:48 +1000

    feat: Install sendgrid and set env variables

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,8 +1,15 @@
-const [envList] = ['MONGODB_URI', 'MONGODB_NAME'] as const;
+const [envList] = [
+  'MONGODB_URI',
+  'MONGODB_NAME',
+  'SENDGRID_API_KEY',
+  'SENDGRID_SENDER',
+] as const;
 
 export const ENV = {
   MONGODB_URI: process.env.MONGODB_URI,
   MONGODB_NAME: process.env.MONGODB_NAME,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  SENDGRID_SENDER: process.env.SENDGRID_SENDER,
 } as { [key in (typeof envList)[number]]: string };
 
 export const isDev = process.env.NODE_ENV === 'development';
